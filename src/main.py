d6e5c3ff6411a11436dd2fb50ac859b15c0a4b4f
Kurt L. Manion <klmanion@gmail.com>
2018-06-11 17:51:41 -0400

    interface

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #main.py
 # created by: Kurt L. Manion
 # on: Mon., 11 June 2018
@@ -25,6 +26,7 @@
 #       - twice, tw
 # without values:
 #   o settings (alias: set, cur, current, curset)
+#	o quit (alias: exit)
 #
 
 # keyword definitions are lists with their aliases
@@ -32,6 +34,20 @@ KW_XDIM = [ "xdim", "rows" ]
 KW_YDIM = [ "ydim", "cols", "columns" ]
 KW_ROT = [ "rot", "rotation", "dir", "direction" ]
 KW_CURSET = [ "settings", "set", "cur", "current", "curset" ]
+KW_HELP = [ "help", "usage" ]
+KW_QUIT = [ "quit", "exit" ]
+KW_LST = [ KW_XDIM, KW_YDIM, KW_ROT, KW_CURSET, KW_HELP, KW_QUIT ]
+
+KW_XDIM_DESC = "X-dimension of the matrix the message is mapped to before" \
+				+"being rotated."
+KW_YDIM_DESC = "Y-dimension of the matrix the message is mapped to before" \
+				+"being rotated."
+KW_ROT_DESC = "Direction to rotate the matrix."
+KW_CURSET_DESC = "Display the current settings."
+KW_HELP = "Dispay usage information."
+KW_QUIT = "Exit the application."
+KW_DESC = [ KW_XDIM_DESC, KW_YDIM_DESC, KW_ROT_DESC, KW_CURSET_DESC,
+			KW_HELP, KW_QUIT ]
 
 DIM_AUTO = [ "auto" ]
 DIM_SQ = [ "sq", "square" ]
@@ -42,15 +58,122 @@ ROT_CCW = [ "ccw", "counter-clockwise" ]
 ROT_TW = [ "tw", "twice" ]
 KW_ROT_VAL = [ ROT_CW, ROT_CCW, ROT_TW ]
 
+# returned by match_keyword if given an invalid keyword
+INV_KW = []
+
 # settings variables
 xdim = DIM_AUTO
 ydim = DIM_AUTO
 rot = ROT_CW
 
-def main():
-    print(rot)
+ver_major = 1
+ver_minor = 0
+ver_patch = 1
+version = str(ver_major)+"."+str(ver_minor)+"."+str(ver_patch)
 
-    return 1
+def encrypt(s):
+	print(s)
+
+def match_keyword(kw):
+	if "=" in kw:
+		kw = kw[:kw.index("=")]
+	kw = kw.strip()
+
+	for i in range(len(KW_LST)):
+		for j in range(len(KW_LST[i])):
+			if KW_LST[i][j] == kw:
+				return KW_LST[i]
+	return INV_KW
+
+def banner():
+	print("transposition-cypher -- v"+version)
+	print("Type `help' for usage information")
+
+def usage(kw):
+	print("foo")
+	if kw is None:
+		print("Input may be either:\n" \
+				+"\t(1) a message to be encrypted with the currect settings"
+				+"\n"
+				+"\t(2) a keyword to change or print the setting")
+		print("To differentiate keywords from messages starting with the" \
+				+"keyword, the keyword must be alone, or, if it has a " \
+				+"corresponding value, be immediately followed by an " \
+				+"equal sign.")
+		print("Type `help' followed by a keyword for aliases, " \
+				+"and its description")
+		for i in range(len(KW_LST)):
+			print('{}\t{}'.format(KW_LST[i][0], KW_DESC[i]))
+	else:
+		kw = match_keyword(kw)
+		if kw == INV_KW:
+			print("invalid keyword")
+		else:
+			print(kw[0], end='')
+
+			if len(kw) > 1:
+				print(" (alias: ", end='')
+				for i in range(1, len(kw)):
+					print(kw[i],", ", sep='', end='')
+				print(")")
+
+			dex = KW_LST.index(kw)
+			print(KW_DESC[dex])
+
+def curset():
+	print("xdim:",str(xdim), sep='\t')
+	print("ydim:",str(ydim), sep='\t')
+	print("rotation:",rot, sep='\t')
+
+def main():
+	running = True
+
+	banner()
+
+	while running:
+		s = input()
+		val = ""
+
+		kw = s.split()[0]
+		if "=" in kw:
+			val = kw[kw.index("=")+1:]
+			val = kw.split()[0]
+		kw = match_keyword(kw)
+
+		if kw != INV_KW:
+			if kw == KW_XDIM:
+				if val.isdigit():
+					xdim = int(val)
+				else:
+					print("non-integer value passed to xdim")
+			elif kw == KW_YDIM:
+				if val.isdigit():
+					ydim = int(val)
+				else:
+					print("non-integer value passed to ydim")
+			elif kw == KW_ROT:
+				if val == "":
+					print("rotation keyword requires a value")
+					usage(kw)
+				else:
+					saved = rot
+					rot = ""
+					for vrot in KW_ROT_VAL:
+						for alias in vrot:
+							if val == alias:
+								rot = vrot
+					if rot == "":
+						rot = saved
+						print("invalid value passed to rotation")
+						usage(kw)
+			elif kw == KW_CURSET:
+				curset()
+			elif kw == KW_HELP:
+				usage()
+			elif kw == KW_QUIT:
+				running = False
+		else:
+			encrypt(s)
 
 main()
 
